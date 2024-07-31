973c95e2e7580f74696b8be1fd02823fe10b5edd
Yassine Azedine <yassine.azedine0711@gmail.com>
2024-07-31 15:16:20 +0100

    first commit

---
+++ utils/config.ts
@@ -0,0 +1,11 @@
+export const homeConfig = {
+  title: 'Welcome to Electro Store | Home',
+  description: 'Find the best products for the cheapest prices.',
+  keywords: 'electronics, cheap electronics, buy electronics',
+};
+
+export const shippingConfig = {
+  title: 'Welcome to Electro Store | Shipping Page',
+  description: 'Find the best products for the cheapest prices.',
+  keywords: 'electronics, cheap electronics, buy electronics',
+};
